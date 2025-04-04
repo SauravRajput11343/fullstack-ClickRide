8f4b1a99c07e21c829234ffc119fa5440aa1ef82
Saurav Rajput <sauravrajput11343@gmail.com>
2025-04-04 22:53:23 +0530

    04-04-2025

--- frontend/src/pages/Vehicle/ManageModel.jsx
+++ frontend/src/pages/Vehicle/ManageModel.jsx
@@ -100,14 +100,7 @@ export default function ManageModel() {
                                 ))}
                             </select>
 
-                            <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors shadow-md hover:shadow-lg ml-auto">
-                                <span className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                    </svg>
-                                    Add Model
-                                </span>
-                            </button>
+                           
                         </div>
                     </div>
 
